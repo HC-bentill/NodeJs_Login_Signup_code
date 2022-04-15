ae8bb0835582ffe298766c6b193c36b8e4493fdd
Henry <henrybentil88@gmail.com>
2022-04-15 01:37:18 +0000

    added cors to codebase

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //mogodb
 require( './config/db' );
 
+const cors = require("cors");
 const app = require( 'express' )(); 
 const port = process.env.PORT || 3000;
 
@@ -9,6 +10,9 @@ const useRouter = require('./api/User')
 //For accepting post from data
 const bodyParser = require( 'express' ).json;
 app.use( bodyParser() );
+
+//allow cross origin
+app.use(cors());
 
 app.use('/user', useRouter)
 
